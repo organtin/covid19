23f0a57741178c40ce9490a1d950f0f14f3387df
organtin <giovanni.organtini@roma1.infn.it>
2020-03-14 19:25:21 +0100

    Added regional analysis

--- trend.py
+++ trend.py
@@ -36,35 +36,56 @@ import re
 ssl._create_default_https_context = ssl._create_unverified_context
 urls = {
     'World' : 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv',
-    'Italy' : 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv'
+    'Italy' : 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv',
+    'Regional': 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv'
     }
 
 url = urls['Italy']
+db = 'Italy'
 country = 'Italy'
+region = ''
 
 # help function
 def covidhelp():
     print('Usage: {} [source] [min]'.format(sys.argv[0]))
-    print('       [source] is optional and can be either "Italy" or "World/<country>"')
+    print('       [source] is optional and can be either "Italy[/<region>]" or "World/<country>"')
     print('       where <country> is the name of the country in which you are')
-    print('       interested in (e.g. "World/China" or "World/Norway")')
+    print('       interested in (e.g. "World/China" or "World/Norway").')
+    print('       The deafult source is Italy. Using Italy/Lazio you use only data for Lazio.')
     print('       [min] identifies the starting point of the plot. Data are considered only')
     print('       starting from the date at which the number of infected begins to be ')
-    print('       higher than [min] (default to 4)')
+    print('       higher than [min] (default to 4).')
 
 # get arguments, if any
 if (len(sys.argv) > 1):
-    if sys.argv[1] != 'Italy':
-        if re.match('^World', sys.argv[1]):
-            url = urls['World']
-            country = sys.argv[1].split("/")[1]
-        else:
-            covidhelp()
-            exit(0)
+    if not re.match('.*/', sys.argv[1]):
+        sys.argv[1] += "/"
+    (db, region) = sys.argv[1].split("/")
+    if db == 'Italy' and len(region) > 0:
+        url = urls['Regional']
+        country = region
+    elif db == 'World': 
+        url = urls['World']
+        country = sys.argv[1].split("/")[1]
+    elif not (db == 'Italy' and len(region) == 0):
+        covidhelp()
+        exit(0)
 
 # download data
 print('Getting data from {} for {}'.format(url.rstrip(), country))
 filename = wget.download(url)
+if db == 'Italy' and len(region) > 0:
+    # remove useless columns from the input file: read it and write a new file
+    fi = open(filename, 'r')
+    ff = open('W' + filename, 'w')
+    for line in fi:
+        if re.search(region, line):
+            # strip useless columns
+            line = re.sub('ITA,[^,]+,[^,]+,[^,]+,[^,]+,', 'ITA,', line)
+            ff.write(line)
+    ff.close()
+    fi.close()
+    os.rename('W' + filename, filename)
 
 # The model function
 def fun(x, a, b):
@@ -85,7 +106,7 @@ f = open(filename, 'r')
 
 w = pd.read_csv(filename)
 os.remove(filename)
-if filename == 'dpc-covid19-ita-andamento-nazionale.csv':
+if db == 'Italy':
     data = w.T.values.tolist()
     head = [dt.datetime.strptime(x, '%Y-%m-%d %H:%M:%S') for x in data[0]]
     Nill = data[10]
@@ -93,7 +114,6 @@ if filename == 'dpc-covid19-ita-andamento-nazionale.csv':
     Nrecovered = data[8]
     Nhospital = data[4]
     Ncritical = data[3]
-    country = 'Italy'
 else:
     data = w.values.tolist()
     head = [dt.datetime.strptime(x, '%m/%d/%y') for x in w.columns[4:]]
@@ -114,6 +134,10 @@ while (Nill[i] < min_val):
     i += 1
 
 Nill = Nill[i:]
+Ndeaths = Ndeaths[i:]
+Nrecovered = Nrecovered[i:]
+Nhospital = Nhospital[i:]
+Ncritical = Ncritical[i:]
 head  = head[i:]
 
 # Compute the logarithm of the data
@@ -142,7 +166,7 @@ print('Fit done: tau = {} +- {}'.format(tau, dtau))
 print('            A = {} +- {}'.format(A, dA))
 
 # A function to make a nice plot
-def myplotfit(plt, x, y, p0, p1, s0, s1, legendPosition=0, legend=None):
+def myplotfit(plt, x, y, p0, p1, s0, s1, legendPosition=0, legend=None, title=None):
     plt.xticks(rotation=45)
     plt.plot(x, np.log(y), 'o')
     xr = range(len(x))
@@ -156,11 +180,13 @@ def myplotfit(plt, x, y, p0, p1, s0, s1, legendPosition=0, legend=None):
     f1 = fun(xr, p0, p1)+s
     f2 = fun(xr, p0, p1)-s
     plt.plot(x, np.log(f0))
+    if title != None:
+        plt.title(title)
     plt.fill_between(x, np.log(f1), np.log(f2), alpha=0.25)
 
 # Create a plot and show data with the model superimposed
 plt.figure(figsize=(12,7))
-myplotfit(plt, head, Nill, A, tau, dA, dtau, .9, 'Fit last 10 points')
+myplotfit(plt, head, Nill, A, tau, dA, dtau, .9, 'Fit last 10 points', title=country)
 plt.show()
 
 # Fit a portion of the data iteratively
@@ -193,6 +219,7 @@ plt.figure(figsize=(12,7))
 plt.plot(head, taudata, 'o')
 plt.ylim(bottom = 0.1)
 plt.xticks(rotation=45)
+plt.title(country)
 plt.ylabel('Characteristic time [d]')
 plt.show()
 
@@ -203,7 +230,7 @@ for i in range(len(lNill) - 1):
     deriv.append(lNill[i + 1]-lNill[i])
     i += 1
 plt.figure(figsize=(12,7))
-plt.title('Evolution of the characteristic time of coronavirus spread')
+plt.title('Evolution of the characteristic time of coronavirus spread\n{}'.format(country))
 plt.plot(head[:-1], deriv, '-o')
 plt.ylabel('$\\frac{d\\tau}{dt}$')
 plt.xticks(rotation=45)
@@ -223,7 +250,7 @@ NillNorm = normalise(Nill)
 
 # fit and plot normalised data
 xr = range(len(NillNorm))
-p, cov = curve_fit(flog, xr, NillNorm, sigma=np.sqrt(NillNorm))
+p, cov = curve_fit(flog, xr, NillNorm, sigma=np.sqrt(NillNorm), maxfev=10000)
 print(p)
 t0 = p[2]
 dt0 = np.sqrt(cov[2][2])
@@ -250,21 +277,76 @@ plt.savefig('logisticfit.png')
 plt.show()
 
 # plot normalised data about other categories
-if country == 'Italy':
+if db == 'Italy':
+    Npop = 60e6
+    
     NdeathNorm = normalise(Ndeaths)
     NrecoNorm = normalise(Nrecovered)
     NhospNorm = normalise(Nhospital)
     NcritNorm = normalise(Ncritical)
+
+    rInfected = max(Nill)/Npop*1000
+    rDeaths = max(Ndeaths)/Npop*1000
+
+    rDeaths2Ill = max(Ndeaths)/max(Nill)*100
+    rReco2Ill = max(Nrecovered)/max(Nill)*100
+    rHosp2Ill = max(Nhospital)/max(Nill)*100
+    rCrit2Ill = max(Ncritical)/max(Nill)*100
+    
     plt.figure(figsize=(12,7))
     plt.title('Evolution of COVID19 spread with time')
-    lgdI, = plt.plot(xr, NillNorm, '-o', label='Infected ({})'.format(max(Nill)))
-    lgdD, = plt.plot(xr, NdeathNorm, '-o', markerfacecolor='w', label='Death ({})'.format(max(Ndeaths)))
-    lgdR, = plt.plot(xr, NrecoNorm, '-o', markerfacecolor='w', label='Recovered ({})'.format(max(Nrecovered)))
-    lgdI, = plt.plot(xr, NhospNorm, '-o', markerfacecolor='w', label='Inpatient ({})'.format(max(Nhospital)))
-    lgcC, = plt.plot(xr, NcritNorm, '-o', markerfacecolor='w', label='Critical ({})'.format(max(Ncritical)))
+    lgdI, = plt.plot(xr, NillNorm, '-o',
+                     label='Infected ({}) {:.2f} permille of population'.format(max(Nill), rInfected))
+    label = 'Death ({}) {:.2f} permille of population/{:.2f}% of infected'.format(max(Ndeaths),
+                                                                                  rDeaths, rDeaths2Ill)
+    plt.plot(xr, NdeathNorm, '-o', markerfacecolor='w', label=label)
+    label = 'Recovered ({}) {:.2f}% of infected'.format(max(Nrecovered), rReco2Ill)
+    plt.plot(xr, NrecoNorm, '-o', markerfacecolor='w', label=label)
+    label = 'Inpatient ({}) {:.2f}% of infected'.format(max(Nhospital), rHosp2Ill)
+    plt.plot(xr, NhospNorm, '-o', markerfacecolor='w', label=label)
+    label = 'Critical ({}) {:.2f}% of infected'.format(max(Ncritical), rCrit2Ill)
+    plt.plot(xr, NcritNorm, '-o', markerfacecolor='w', label=label)
     plt.annotate('[{}] Data up to {}'.format(country, dt.date.today()), (0.1, 0.6), xycoords='axes fraction')
     plt.xlabel('t [d]')
     plt.ylabel('N/N$_{max}$')
     plt.legend()
     plt.savefig('normalised.png')
+    plt.show()
+
+    mortality = []
+    em = []
+    inNeedForHospital = []
+    eh = []
+    inNeedForIntensiveCareUnit = []
+    eicu = []
+    for i in range(len(Nill)):
+        N2 = Nill[i]
+        N1 = Ndeaths[i]
+        e1 = np.sqrt(N1)/N2
+        e2 = N1*np.sqrt(N2)/N2**2
+        e = np.sqrt(e1**2 + e2**2)
+        mortality.append(N1/N2)
+        em.append(e)
+        N1 = Nhospital[i]
+        e1 = np.sqrt(N1)/N2
+        e2 = N1*np.sqrt(N2)/N2**2
+        e = np.sqrt(e1**2 + e2**2)
+        inNeedForHospital.append(N1/N2)
+        eh.append(e)
+        N1 = Ncritical[i]
+        e1 = np.sqrt(N1)/N2
+        e2 = N1*np.sqrt(N2)/N2**2
+        e = np.sqrt(e1**2 + e2**2)
+        eicu.append(e)
+        inNeedForIntensiveCareUnit.append(N1/N2)
+
+    plt.figure(figsize=(12,7))
+    plt.title('COVID19: evolution of the consequences of infection')
+    plt.errorbar(xr, mortality, label='mortality', yerr=em, fmt='-o')
+    plt.errorbar(xr, inNeedForHospital, yerr=eh, fmt='-o', label='in need for hospital')
+    plt.errorbar(xr, inNeedForIntensiveCareUnit, yerr=eicu, fmt='-o', label='in need for intensive care unit')
+    plt.xlabel('t [d]')
+    plt.ylabel('N/N$_{infected}$')
+    plt.legend()
+    plt.savefig('consequences.png')
     plt.show()
