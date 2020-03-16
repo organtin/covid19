ca27fbac7f4d9dd7ab96e50aba8c6fdb2f5bb317
organtin <giovanni.organtini@roma1.infn.it>
2020-03-16 14:26:10 +0100

    few improvements

--- compare.py
+++ compare.py
@@ -1,5 +1,13 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+from scipy.optimize import curve_fit
+
+relative = True
+islog = True
+
+def line(x, a, b):
+    return a*x+b
 
 w = pd.read_csv('saved.csv')
 data = w.T.values.tolist()
@@ -18,14 +26,27 @@ for r in regions:
                 t += 1
                 x.append(t)
                 y.append(ill)
-    plt.plot(x, y, label = r)
     if len(y) > 0 and y[-1] > 1000:
         plt.annotate(r, (x[-3], y[-2]))
+        ymax = y[-1]
+        if relative:
+            y = [i/ymax for i in y]
+        if islog:
+            y = [np.log(i) for i in y]
+            p, cov = curve_fit(line, x, y)
+        plt.plot(x, y, label = r)
+        k = int(len(x)/2)
+        plt.annotate('slope = {:.2f}'.format(p[0]), (x[k], y[k]))
     x.clear()
     y.clear()
 plt.legend()
 plt.xlabel('time from t$_0$ where t$_0$ is the time at which N$_{inf}>100$ [d]')
-plt.ylabel('N$_{inf}$')
+ylabel = 'N$_{inf}$'
+if islog:
+    ylabel = '$\log{N_{inf}}$'
+if relative:
+    ylabel += ' (relative)'
+plt.ylabel(ylabel)
 plt.xticks(rotation = 45)
 plt.show()
         
