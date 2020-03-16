d1d6b04bf69e5db8afe164902119943d11bcfef6
organtin <giovanni.organtini@roma1.infn.it>
2020-03-16 11:48:01 +0100

    bugs fixed. Few improvements

--- covid-simulation.py
+++ covid-simulation.py
@@ -3,18 +3,28 @@ import math
 from matplotlib import pyplot as plt
 from matplotlib import animation
 
+# parameters of the simulation
+N = 50        # number of inhabitants
+dt = 0.5       # time step (a.u.) 
+size = 80      # size of the town (occupancy = N/size**2)
+frames = 1000  # duration of the simulation
+recoT = 150    # recovery time (in a.u.)
+P = 0.75       # probability to infect other people
+save = True
+
 # a class representing a person
 class Person:
     _position = [0, 0]
     _velx = 0
     _vely = 0
     _town = ''
-    _ill = 0
+    _ill= 0
     _illtime = 0
-    _recoveryTime = 200
+    _recoveryTime = 0
     _P = 0
+    _dieP = 0
 
-    def __init__(self, P, initialP=0.02):
+    def __init__(self, P, initialP = 0.03, dieP = 0.045, recoveryTime = recoT):
         self._position[0] = np.random.uniform()
         self._position[1] = np.random.uniform()
         theta = np.random.uniform(0, 2*math.pi)
@@ -23,6 +33,8 @@ class Person:
         self._P = initialP
         self.setAsIll()
         self._P = P
+        self._dieP = dieP/recoT
+        self._recoveryTime = recoT
     def isIll(self):
         ret = False
         if self._ill == 1:
@@ -47,20 +59,17 @@ class Person:
     def setAsHealthy(self):
         self._ill = 0
         self._illtime = 0
-    def step(self, dt):
-        # perform a simulation step
-        # if it is ill increase the corresponding time
-        if self.isIll():
-            self._illtime += 1
+        # once recovered it cannot be infected anymore
+        self._P = 0
+    def tryToRecover(self):
         # check if it recovers
-        recoveryTime = self._recoveryTime + np.random.normal(scale = 15)
-        if self._illtime > recoveryTime:
-            self.setAsHealthy()
-        # remove it from the current position
-        self._town.removePerson(self)
-        # compute next position
-        self._position[0] += dt*self._velx
-        self._position[1] += dt*self._vely
+        if self.isIll():
+            recoveryTime = self._recoveryTime + np.random.normal(scale = 15)
+            if self._illtime > recoveryTime:
+                self.setAsHealthy()
+        else:
+            self._illtime += 1
+    def bounce(self):
         # check if it is at the border
         s = self._town.size() - 1
         if self._position[0] >= s:
@@ -75,21 +84,50 @@ class Person:
         if self._position[1] < 0:
             self._position[1] = 0
             self._vely = -self._vely
+    def meet(self, p):
+        if p != None:
+            # the velocity of the CM
+            (Vx, Vy) = (self._velx/2 + p._velx/2, self._vely/2 + p._vely/2)
+            # the velocity of self in the CM after the collision
+            (Vx1, Vy1) = (-(self._velx-Vx), -(self._vely-Vy))
+            # the velocity of p in the CM after the collision
+            (Vx2, Vy2) = (-(p._velx-Vx), -(p._vely-Vy))
+            # the velocity of self in the laboratory
+            (Vx1, Vy1) = (Vx1 + Vx, Vy1 + Vy)
+            # the velocity of p in the laboratory
+            (Vx2, Vy2) = (Vx2 + Vx, Vy2 + Vy)
+            self._velx = Vx1
+            self._vely = Vy1
+            p._velx = Vx2
+            p._vely = Vy2
+            # check if self can transmit infection to p
+            if self.isIll():
+                p.setAsIll()
+    def step(self, dt):
+        # perform a simulation step
+        self.tryToRecover()
+        # remove it from the current position
+        self._town.removePerson(self)
+        # compute next position
+        self._position[0] += dt*self._velx
+        self._position[1] += dt*self._vely
+        self.bounce()
         # move it to the current position
         self._town.putPerson(self)
         # check if there is anyone in the vicinity
         x = int(self._position[0])
         y = int(self._position[1])
+        s = self._town.size() - 1
         for ix in [max(0, x - 1), x, min(s, x + 1)]:
             for iy in [max(0, y - 1), y, min(s, y +1)]:
                 if ix != x and iy != y:
                     p = self._town.personAt(ix, iy)
-                    if p != None:
-                        self.invertVelocity()
-                        p.invertVelocity()
-                        # check if it can transmit infection
-                        if self.isIll():
-                            p.setAsIll()
+                    self.meet(p)
+        # see if it must die
+        if self.isIll():
+            r = np.random.uniform()
+            if r < self._dieP:
+                self._town.population().remove(self)
 
 # a town class                        
 class Town:
@@ -129,14 +167,9 @@ class Town:
                 n += 1
         return n
     
-N = 500
-dt = 0.5
-size = 200
-frames = 2000
-
 t = Town('COVIDVille', size)
 for i in range(N):
-    t.add(Person(0.75))
+    t.add(Person(P))
 
 print('======== ' + t.name())
 p = t.population()
@@ -153,7 +186,7 @@ ax.get_xaxis().set_visible(False)
 ay.set_xlim(0, frames / 0.5)
 ay.set_ylim(0, N)
 ay.get_xaxis().set_visible(False)
-ay.get_yaxis().set_visible(False)
+#ay.get_yaxis().set_visible(False)
 
 x = []
 y = []
@@ -168,8 +201,9 @@ ills = []
 time.append(0)
 ills.append(t.nIll())
 rills = ills[-1]/len(p)*100
-txt = plt.text(0.01, 0.03, 't: {} Infected: {} ({:.1f} %)'.format(1, ills, rills),
-               fontsize = 13, transform=fig.transFigure)
+txt = plt.text(0.01, 0.03,
+               't: {} P: {} Infected: {} ({:.1f} %)'.format(1, len(p), ills, rills),
+               fontsize = 10, transform=fig.transFigure)
 
 def animate(i):
     t.step(dt)
@@ -186,15 +220,23 @@ def animate(i):
             health.append(20)
     scat.set_offsets(np.c_[x, y])
     scat.set_array(np.array(health))
-    time.append(i)
-    ills.append(t.nIll())
-    rills = ills[-1]/len(p)*100
-    txt.set_text('t: {} Infected: {} ({:.1f} %)'.format(i + 1, ills[-1], rills))
+    if i % (frames/100) == 0:
+        time.append(i)
+        ills.append(t.nIll())
+    rills = 0
+    if len(p) > 0:
+        rills = ills[-1]/len(p)*100
+    txt.set_text('t: {} P: {} Infected: {} ({:.1f} %)'.format(i + 1, len(p), ills[-1], rills))
+    M = max(ills)
+    ay.set_ylim(0, M*1.1)
     ay.plot(time, ills, 'b-')
+    print('t: {:5d} p: {:6d} p/N: {:3.0f} % i/p: {:5.2f} %'.
+          format(i, len(p), len(p)/N*100, ills[-1]/len(p)*100))
     return scat
 
 anim = animation.FuncAnimation(fig, animate, interval=20, frames=frames, repeat = False)
 
-#anim.save('covid_animation_n200x1000.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+if save:
+    anim.save('covid_animation_n{}x{}.mp4'.format(N, size), fps=30, extra_args=['-vcodec', 'libx264'])
 
 plt.show()
